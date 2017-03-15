001595928b030981c63191b503922fab5baaab4c
ardramli <adliramli88@gmail.com>
2017-03-15 10:57:39 +0800

    Initial Commit

---
+++ FunWORDS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FunWORDS
+//
+//  Created by ardMac on 15/03/2017.
+//  Copyright © 2017 ardMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
